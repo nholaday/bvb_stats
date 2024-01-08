e1b6a8ffaae1014ffa5af5c8634b6e4b6994b18b
Nic Holaday <nic@storeease.com>
2024-01-07 20:38:33 -0800

    use an moving index to mark place in intepretation.  Add intepret rally

--- bvbstats.py
+++ bvbstats.py
@@ -1,4 +1,5 @@
 import argparse
+import re
 from pathlib import Path
 from pprint import pprint
 
@@ -57,62 +58,113 @@ def update_stats(stats, player, score, action):
 
 def intepret_serve(stats, partner, line):
     """Inteprets the stats for serve receive chunk"""
+    # actions = re.search(r"([A-Z])(s)([A-Z])(-?\d)([p,h])(-?\d)([s,h])(-?\d)(h)", line)
     # TODO: score for attacking can be -1, account for 2 character score by
     # using regex or by moving an index instead of using an absolute index
+    # could have a function that moves the index until it finds the action then interprets the
+    # score behind it.
 
+    # index that progresses through the line
+    i = 0
     # Serve
     # 1st character is the server, then "s"
     # 3rd character is the receiver or "e" for service error or "a" for ace down the middle
-    server = line[0]
-    receiver = line[2]
+    server = line[i]
+    receiver = line[i + 2]
     if receiver == "e":
         stats[server]["serve"].append(0)
         return
     elif receiver == "a":
         stats[server]["serve"].append(4)
         return
+    setter = partner[receiver]
+    i += 3
 
     # Receive
-    # 4th character is score for pass, then "p" OR
-    # 4th character is score for attack(hit), then "h"
-    if line[4] == "p":
-        stats[receiver]["pass"].append(line[3])
-        stats[server]["serve"].append(pass_to_serve_score[line[3]])
-    elif line[4] == "h":
-        stats[receiver]["attack"].append(line[3])
+    # 5th character is score for pass, then "p" OR
+    # 5th character is score for attack(hit), then "h"
+    if line[i + 1] == "p":
+        stats[receiver]["pass"].append(line[i])
+        stats[server]["serve"].append(pass_to_serve_score[line[i]])
+    elif line[i + 1] == "h":
+        stats[receiver]["attack"].append(line[i])
         stats[server].append(1)
         return
+    elif line[i + 2] == "h":
+        stats[receiver]["attack"].append(line[i : i + 2])
+        stats[server].append(1)
+        return
+    i += 2
 
     # Set
-    # 6th character is score for set, then "s" OR
-    # 6th character is score for attack(hit) on 2, then "h"
-    if line[6] == "s":
-        stats[partner[receiver]]["set"].append(line[5])
-        if line[5] == "0":
+    # 7th character is score for set, then "s" OR
+    # 7th character is score for attack(hit) on 2, then "h"
+    if line[i + 1] == "s":
+        stats[setter]["set"].append(line[i])
+        if line[i] == "0":
             return
-    elif line[6] == "h":
-        stats[partner[receiver]]["attack"].append(line[5])
+    elif line[i + 1] == "h":
+        stats[setter]["attack"].append(line[i])
         return
+    elif line[i + 2] == "h":
+        stats[setter]["attack"].append(line[i : i + 2])
+        return
+    i += 2
 
     # Attack
     # 8th character is score for attack, then "h"
-    if line[8] == "h":
-        stats[receiver]["attack"].append(line[7])
-    elif line[9] == "h":
-        stats[receiver]["attack"].append(line[7:9])
+    if line[i + 1] == "h":
+        stats[receiver]["attack"].append(line[i])
+    elif line[i + 1] == "h":
+        stats[receiver]["attack"].append(line[i : i + 2])
 
 
 def interpret_rally(stats, partner, line):
     """Interprets the stats for rally chunks"""
-    # Block or dig(pass)
+    i = 0
     # First character is the blocker or digger
+    first_touch = line[i]
+    i += 1
+
+    # Block or dig(pass)
+    if line[i + 1] == "p":
+        stats[first_touch]["dig"].append(line[i])
+    elif line[i + 1] == "b":
+        stats[first_touch]["block"].append(line[i])
+        if line[i] == "1":
+            return
+    elif line[i + 1] == "h":
+        stats[first_touch]["attack"].append(line[i])
+        return
+    elif line[i + 2] == "h":
+        stats[first_touch]["attack"].append(line[i : i + 2])
+        return
+    i += 2
 
     # Set
     # Ball can be set by either player in case of a block
     # Default to partner of blocker/digger unless specified
+    sec_touch = partner[first_touch]
+
+    if line[i + 1] == "s":
+        stats[sec_touch]["set"].append(line[i])
+    elif line[i + 1] == "h":
+        stats[sec_touch]["attack"].append(line[i])
+        return
+    elif line[i + 2] == "s":
+        sec_touch = line[i]
+        stats[sec_touch]["set"].append(line[i + 1])
+        i += 1
+    elif line[i + 2] == "h":
+        stats[sec_touch]["attack"].append(line[i : i + 2])
+        return
+    i += 2
 
     # Attack
-    pass
+    if line[i + 1] == "h":
+        stats[sec_touch]["attack"].append(line[i])
+    elif line[i + 2] == "h":
+        stats[sec_touch]["attack"].append(line[i : i + 2])
 
 
 def main():
@@ -128,22 +180,25 @@ def main():
     args = get_args()
     filename = Path(args.path)
     with open(filename, "r") as file:
-        for line in file:
-            print(line, end="")
-            if line[0] == "#":
+        for i, line in enumerate(file):
+            # print(line, end="")
+            if i == 0:
                 title = line
                 continue
 
             chunks = line.split(" ")
             for i, chunk in enumerate(chunks):
-                if i == 0:
-                    intepret_serve(stats, partner, line)
-                else:
-                    interpret_rally(stats, partner, line)
-            pprint(stats)
+                try:
+                    if i == 0:
+                        intepret_serve(stats, partner, chunk)
+                    else:
+                        interpret_rally(stats, partner, chunk)
+                except Exception as e:
+                    print(f"Unable to parse {chunk} in {line}{e}")
+            # pprint(stats)
 
-    # print(title)
-    # pprint(stats)
+    print(title)
+    pprint(stats)
     # TODO: calculate stats based on raw data
     # TODO: intepret on 2 stats
 
@@ -158,3 +213,4 @@ if __name__ == "__main__":
 # could do it by seeing an action then crawling nearby actions by moving the index
 # to find the needed info
 # TODO: record the score of the game
+# TODO: deal with overpasses/sets, they're not attacks but they're not errors
